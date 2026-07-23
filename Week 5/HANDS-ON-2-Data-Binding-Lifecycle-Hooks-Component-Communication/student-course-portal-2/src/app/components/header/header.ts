d202e28ed3594de63068c095318f591fee648df1
PratapSakthivel <pratapssakthivel@gmail.com>
2026-07-23 08:05:46 +0530

    Implement HANDS-ON-2: Data Binding, Lifecycle Hooks & Component Communication - All tasks complete

--- Week 5/HANDS-ON-2-Data-Binding-Lifecycle-Hooks-Component-Communication/student-course-portal-2/src/app/components/header/header.ts
+++ Week 5/HANDS-ON-2-Data-Binding-Lifecycle-Hooks-Component-Communication/student-course-portal-2/src/app/components/header/header.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
